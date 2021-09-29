2b979585ed04b2150b042abdd7b58f27ab699d80
ssandra <1617218599@qq.com>
2021-09-29 21:30:56 +0800

    use state

--- src/App.js
+++ src/App.js
@@ -2,19 +2,67 @@
 import "./App.css";
 import Title from "./components/title/title";
 import HoverBox from "./components/HoverBox/HoverBox";
-function App() {
-  return (
-    <div className="App">
-      <Title text="A-SOUL Digits"></Title>
-      <div className="hover-box-container">
-          <HoverBox name="ava"></HoverBox>
-          <HoverBox name="bella"></HoverBox>
-          <HoverBox name="carol"></HoverBox>
-          <HoverBox name="diana"></HoverBox>
-          <HoverBox name="eileen"></HoverBox>
-        </div>
-    </div>
-  );
+import React from "react";
+import axios from "axios";
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      A: {
+        name: "ava",
+        bar: "向晚AVA",
+        data:{}
+      },
+      B: {
+        name: "bella",
+        bar: "贝拉BELLA",
+        data:{}
+      },
+      C: {
+        name: "carol",
+        bar: "珈乐CAROL",
+        data:{}
+      },
+      D: {
+        name: "diana",
+        bar: "嘉然DIANA",
+        data: {}
+      },
+      E: {
+        name: "eileen",
+        bar: "乃琳EILEEN",
+        data: {}
+      }
+    }
+  }
+
+  componentDidMount(){
+    axios.get("api/x/relation/stat",{
+      params:{
+        vmid: 672328094
+      }
+    }).then((res)=>{
+      console.log(res)
+    }).catch((e)=>{
+      console.error(e)
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Title text="A-SOUL Digits"></Title>
+        <div className="hover-box-container">
+            <HoverBox name="ava" info={this.state.A}></HoverBox>
+            <HoverBox name="bella" info={this.state.B}></HoverBox>
+            <HoverBox name="carol" info={this.state.C}></HoverBox>
+            <HoverBox name="diana" info={this.state.D}></HoverBox>
+            <HoverBox name="eileen" info={this.state.E}></HoverBox>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
