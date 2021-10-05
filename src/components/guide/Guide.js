2ae6850fe128772c81c52da110e4b984131c38a6
ssandra <1617218599@qq.com>
2021-10-05 19:36:48 +0800

    update guide

--- src/components/guide/Guide.js
+++ src/components/guide/Guide.js
@@ -1,47 +1,55 @@
 import React from "react";
-import pic from "../../assets/guide.png"
-import "./Guide.css"
+import pic from "../../assets/guide.png";
+import "./Guide.css";
 
-class Guide extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            showGuide: false
-        }
-        this.handleEnter = this.handleEnter.bind(this)
-        this.handleLeave = this.handleLeave.bind(this)
+class Guide extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showGuide: false,
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleLeave = this.handleLeave.bind(this);
+  }
+
+  handleEnter() {
+    this.setState({
+      showGuide: true,
+    });
+  }
+
+  handleLeave() {
+    this.setState({
+      showGuide: false,
+    });
+  }
+
+  get showClass() {
+    if (this.state.showGuide) {
+      return "show-guide";
+    } else {
+      return "hide-guide";
     }
+  }
 
-    handleEnter(){
-        this.setState({
-            showGuide: true
-        })
-    }
-
-    handleLeave(){
-        this.setState({
-            showGuide: false
-        })
-    }
-
-    get showClass(){
-        if(this.state.showGuide){
-            return 'show-guide'
-        }else{
-            return 'hide-guide'
-        }
-    }
-
-    render(){
-        return(
-            <div className="guide">
-                <img onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave} className="img" src={pic} alt=""></img>
-                <div className={this.showClass}>每次展开信息的时候会刷新，点一下粉丝数可以详细显示</div>
-            </div>
-        )
-
-
-    }
+  render() {
+    return (
+      <div className="guide">
+        <img
+          onMouseEnter={this.handleEnter}
+          onMouseLeave={this.handleLeave}
+          className="img"
+          src={pic}
+          alt=""
+        ></img>
+        <div className={this.showClass}>
+          <span>每次展开信息的时候会刷新</span>
+          <span>点一下粉丝数可以详细显示</span>
+          <span>直接右键粉丝数复制</span>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Guide
+export default Guide;
