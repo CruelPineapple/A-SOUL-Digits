f282a5f3cb8e20a2ad43266ca79db65d3f1e151a
ssandra <1617218599@qq.com>
2021-11-21 20:56:04 +0800

    to function compponent

--- src/App.js
+++ src/App.js
@@ -1,60 +1,48 @@
 import "./App.css";
 import Title from "./components/title/title";
-import HoverBox from "./components/HoverBox/HoverBox";
+
 import Guide from "./components/guide/Guide"
+import BoxList from "./components/BoxList/BoxList";
 import React from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      A: {
-        name: "ava",
-        bar: "向晚AVA",
-        vmid: 672346917,
-      },
-      B: {
-        name: "bella",
-        bar: "贝拉BELLA",
-        vmid: 672353429,
-      },
-      C: {
-        name: "carol",
-        bar: "珈乐CAROL",
-        vmid: 351609538,
-      },
-      D: {
-        name: "diana",
-        bar: "嘉然DIANA",
-        vmid: 672328094,
-      },
-      E: {
-        name: "eileen",
-        bar: "乃琳EILEEN",
-        vmid: 672342685,
-      },
-    };
-  }
+const infoList = [
+  {
+    name: "ava",
+    bar: "向晚AVA",
+    vmid: 672346917,
+  },
+  {
+    name: "bella",
+    bar: "贝拉BELLA",
+    vmid: 672353429,
+  },
+  {
+    name: "carol",
+    bar: "珈乐CAROL",
+    vmid: 351609538,
+  },
+  {
+    name: "diana",
+    bar: "嘉然DIANA",
+    vmid: 672328094,
+  },
+  {
+    name: "eileen",
+    bar: "乃琳EILEEN",
+    vmid: 672342685,
+  },
+]
 
-  componentDidMount() {
-    
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Title text="A-SOUL Digits"></Title>
-        <div className="hover-box-container">
-          <HoverBox name="ava" info={this.state.A}></HoverBox>
-          <HoverBox name="bella" info={this.state.B}></HoverBox>
-          <HoverBox name="carol" info={this.state.C}></HoverBox>
-          <HoverBox name="diana" info={this.state.D}></HoverBox>
-          <HoverBox name="eileen" info={this.state.E}></HoverBox>
-        </div>
-        <Guide></Guide>
+function App() {
+  return (
+    <div className="App">
+      <Title text="A-SOUL Digits"></Title>
+      <div className="hover-box-container">
+        <BoxList list={infoList}/>
       </div>
-    );
-  }
+      <Guide></Guide>
+    </div>
+  );
 }
 
 export default App;
